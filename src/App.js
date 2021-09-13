e3f8e757b97c9b82473ce06e8287546cc63a78bc
sahil <sahilkeshav1997@gmail.com>
2021-09-13 14:50:29 +0530

    feat: added preloader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import Form from "./Form";
-import News from "./News";
-import Buttons from "./Buttons";
+import React, { useEffect, useState } from 'react'
+import Form from './Form'
+import News from './News'
+import Buttons from './Buttons'
+import $ from 'jquery'
+
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    $(function () {
+      $('.loader').delay(2000).fadeOut('slow')
+      $('#overlayer').delay(2000).fadeOut('slow')
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <>
-      <Form />
-      <Buttons />
-      <News />
+      <div id='overlayer'>
+        <span className='loader'>
+          <span className='loader-inner'></span>
+        </span>
+      </div>
+      {!loading && (
+        <>
+          <Form />
+          <Buttons />
+          <News />
+        </>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
